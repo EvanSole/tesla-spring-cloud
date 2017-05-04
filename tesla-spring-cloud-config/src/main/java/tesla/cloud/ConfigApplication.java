aa3c77000f5e270d29c7ec2bcc3b843eb3bec76b
Edwin <Edwin.wang@Edwin-MacBook-Pro.local>
2017-05-04 14:47:54 +0800

    init config

---
+++ tesla-spring-cloud-config/src/main/java/tesla/cloud/ConfigApplication.java
@@ -0,0 +1,13 @@
+package tesla.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConfigApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
