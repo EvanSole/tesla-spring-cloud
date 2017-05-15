a4a1fa1ad07f6cd23e9bff8542af729b782a2533
edwin.wang <edwin.wang@huamengtong.com>
2017-05-15 16:41:29 +0800

    commit docker

--- TeslaPlatform/tesla-books-service/src/main/java/com/tesla/cloud/controller/BookController.java
+++ TeslaPlatform/tesla-books-service/src/main/java/com/tesla/cloud/controller/BookController.java
@@ -1,10 +1,19 @@
 package com.tesla.cloud.controller;
 
+import com.tesla.cloud.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
 
+    @Autowired
+    BookService bookService;
 
+    @GetMapping("/books")
+    public Object books(){
+      return  bookService.getAllBooks();
+    }
 
 }
